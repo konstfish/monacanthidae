4ebf9306af3635a9060da9ddfdf44eef2d6da22f
David Fischer <david.fishical@gmail.com>
2019-12-18 21:17:36 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+if [ "$1" == "b" ]; then
+  docker build -t monacanthidae .
+fi
+
+docker container stop monacanthidae
+docker container rm monacanthidae
 docker run -v /stor/pictures/:/data -d -p 6969:3000 --name monacanthidae monacanthidae:latest
